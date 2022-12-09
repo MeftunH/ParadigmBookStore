7933ec7a56850da16899f21ca2e1623388ce2fd4
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-09 23:54:36 +0400

    add category type enum

---
+++ src/main/java/com/example/paradigmbookstore/dto/CategoryType.java
@@ -0,0 +1,12 @@
+package com.example.paradigmbookstore.dto;
+
+public enum CategoryType {
+    SCIENCE("Science"),
+    FICTION("Fiction"),
+    ;
+
+    private final String value;
+    CategoryType(String value) {
+        this.value = value;
+    }
+}
