e7f678577adb32f064a39ffa234f0682863b1c90
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-10 23:58:34 +0400

    set maven

---
+++ src/main/java/com/example/paradigmbookstore/ParadigmBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.paradigmbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadigmBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadigmBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paradigmbookstore/ParadigmBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paradigmbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadigmBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
