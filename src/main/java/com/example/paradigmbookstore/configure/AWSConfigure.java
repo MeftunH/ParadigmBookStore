20d10815228e165db1afa0bd75d040f0b9c846d6
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-12 23:50:47 +0400

    aws config

---
+++ src/main/java/com/example/paradigmbookstore/configure/AWSConfigure.java
@@ -0,0 +1,19 @@
+package com.example.paradigmbookstore.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfigure {
+    public static final String REGION = "us-east-1";
+    public static final String ACCESS_KEY = "AKDOALMQOPAOAASDF";
+    public static final String SECRET_KEY = "aekpqmxO+wmnkIf2dgmsckjwnfuropwsdf";
+
+    @Bean
+    public AmazonS3 s3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(REGION)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+                .build();
+    }
+}
