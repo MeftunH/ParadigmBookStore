2cc614678f6bd101c00a6f429ed2dfcb1ffe92ef
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-12 23:51:07 +0400

    add book model entity annotation

--- src/main/java/com/example/paradigmbookstore/model/Book.java
+++ src/main/java/com/example/paradigmbookstore/model/Book.java
@@ -1,6 +1,25 @@
 package com.example.paradigmbookstore.model;
 
-public class Book {
-    private Long id;
+import com.example.paradigmbookstore.dto.CategoryType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+public class Book {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String title;
+    private String authorName;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
