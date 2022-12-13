7738a0d1c4fc9cf2c07712a14f4458175a7cb10c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-13 23:44:56 +0400

    add book relation on category

--- src/main/java/com/example/paradigmbookstore/model/Category.java
+++ src/main/java/com/example/paradigmbookstore/model/Category.java
@@ -1,6 +1,7 @@
 package com.example.paradigmbookstore.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -8,4 +9,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+    @OneToMany(mappedBy = "category")
+    private List<Book> books;
+
 }
