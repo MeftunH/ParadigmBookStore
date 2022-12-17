357902fb191b8cabbd30a01d76497ab7dad50f93
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-17 23:57:32 +0400

    add time stamps to base entity
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/example/paradigmbookstore/model/BaseEntity.java
+++ src/main/java/com/example/paradigmbookstore/model/BaseEntity.java
@@ -1,10 +1,12 @@
 package com.example.paradigmbookstore.model;
 
 import lombok.Data;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @MappedSuperclass // This annotation is used to indicate that a class is a base class for an entity hierarchy.
 @Data
@@ -12,4 +14,7 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private Long id;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
