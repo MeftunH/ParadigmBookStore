5bde31cbd9b0bb8d5244f66f99e0cb5796c286e2
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-18 11:06:54 +0400

    set category repository
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

---
+++ src/main/java/com/example/paradigmbookstore/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.paradigmbookstore.repository;
+
+import com.example.paradigmbookstore.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+
+}
