3d69fce27580badc521d78b02fb991afd56b3e7e
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-19 23:56:28 +0400

    set book save request
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

---
+++ src/main/java/com/paradigmbookstore/request/book/SaveBookRequest.java
@@ -0,0 +1,17 @@
+package com.paradigmbookstore.request.book;
+
+import com.paradigmbookstore.model.BookStatus;
+import com.paradigmbookstore.model.Category;
+import com.paradigmbookstore.model.Image;
+
+import javax.persistence.*;
+import java.io.File;
+
+public final class SaveBookRequest {
+    private String title;
+    private String authorName;
+    private BookStatus status;
+    private Category category;
+    private Integer lastPageNumber;
+    private File file;
+}
