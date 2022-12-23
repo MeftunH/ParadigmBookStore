1a31ac29337288fbaa380c798e2746666d3c48cb
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-23 23:42:04 +0400

    add load category functionality
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

---
+++ src/main/java/com/paradigmbookstore/service/CategoryService.java
@@ -0,0 +1,16 @@
+package com.paradigmbookstore.service;
+
+import com.paradigmbookstore.model.Category;
+import com.paradigmbookstore.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public Category loadCategory(Long id){
+        return categoryRepository.findById(id).orElseThrow();
+    }
+}
