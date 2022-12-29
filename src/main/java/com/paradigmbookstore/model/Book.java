f983026fddc06bccf2d180dcb97b95ac506b6063
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-29 23:42:23 +0300

    add id and publisher name
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/model/Book.java
+++ src/main/java/com/paradigmbookstore/model/Book.java
@@ -12,6 +12,7 @@ import javax.persistence.*;
 public class Book extends BaseEntity {
     private String title;
     private String authorName;
+    private String publisherName;
 
     @Enumerated(value = EnumType.STRING)
     private BookStatus status;
