ee2874bb55b05721197c37e5ebf279c5218c0b2c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-30 23:38:24 +0300

    FIX:
    add missed not null and not blank annotations
    
    Closes need validation on fields
    
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/request/book/SaveBookRequest.java
+++ src/main/java/com/paradigmbookstore/request/book/SaveBookRequest.java
@@ -6,16 +6,27 @@ import com.paradigmbookstore.model.Image;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.File;
 
 @Data
 public final class SaveBookRequest {
+
+    @NotBlank
     private String title;
+    @NotBlank
     private String authorName;
+    @NotBlank
+    private String publisherName;
+    @NotNull
     private BookStatus status;
-    private Category category;
+    @NotNull
     private Integer lastPageNumber;
-    private File file;
+    private File image;
+    @NotNull
     private Long categoryId;
+    @NotNull
     private Integer totalPageNumber;
 }
