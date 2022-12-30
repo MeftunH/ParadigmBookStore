b0664fa59c3b198fb3b96970d1af8ac397a498b0
Maftun Hashimli <maftunhashimli@gmail.com>
2022-12-30 23:59:25 +0300

    add title and image url variables set superbuilder
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/dto/BookResponse.java
+++ src/main/java/com/paradigmbookstore/dto/BookResponse.java
@@ -3,16 +3,20 @@ package com.paradigmbookstore.dto;
 import com.paradigmbookstore.model.BookStatus;
 import com.paradigmbookstore.model.Category;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 import java.io.File;
 
 @Data
+@SuperBuilder
 public class BookResponse {
     private String authorName;
+    private String title;
     private BookStatus status;
     private Category category;
     private Integer lastPageNumber;
     private File file;
     private Long categoryId;
     private Integer totalPageNumber;
+    private String imageUrl;
 }
