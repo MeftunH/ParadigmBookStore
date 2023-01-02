1ba05b8cc005bbc59ac3707a3acfd672a8e7b6d9
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-03 02:57:29 +0300

    set search by category functionality
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/service/BookListService.java
+++ src/main/java/com/paradigmbookstore/service/BookListService.java
@@ -1,9 +1,12 @@
 package com.paradigmbookstore.service;
 
 import com.paradigmbookstore.dto.BookResponse;
+import com.paradigmbookstore.dto.CategoryType;
+import com.paradigmbookstore.model.Book;
+import com.paradigmbookstore.model.Category;
 import com.paradigmbookstore.repository.BookRepository;
-import com.paradigmbookstore.repository.CategoryRepository;
 import com.paradigmbookstore.request.book.BookSearchRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -11,25 +14,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class BookListService
-{
-    private BookRepository bookRepository;
-    private CategoryRepository categoryRepository;
+@RequiredArgsConstructor
+public class BookListService {
+    private final BookRepository bookRepository;
+    private final CategoryService categoryService;
+
+    private static BookResponse getBuild(Book model) {
+        return BookResponse.builder()
+                .authorName(model.getAuthorName())
+                .title(model.getTitle())
+                .imageUrl(model.getImage()
+                .getImageUrl())
+                .build();
+    }
 
     //TODO: Convert to request param
-    public List<BookResponse> listBooks(BookSearchRequest bookSearchRequest)
-    {
-       return bookRepository.findAll(PageRequest.of(bookSearchRequest.getPage(), bookSearchRequest.getSize()))
-               .getContent()
-               .stream()
-               .map(model ->
-                   BookResponse.builder()
-                            .authorName(model.getAuthorName())
-                            .title(model.getTitle())
-                            .imageUrl(model.getImage().getImageUrl())
-                            .build())
-                            .collect(Collectors.toList());
+    public List<BookResponse> listBooks(BookSearchRequest bookSearchRequest) {
+        return bookRepository.findAll(PageRequest.of(bookSearchRequest.getPage(), bookSearchRequest.getSize()))
+                .getContent()
+                .stream()
+                .map(BookListService::getBuild)
+                .collect(Collectors.toList());
+    }
 
+    public List<BookResponse> searchByCategory(CategoryType categoryType) {
+        Category category = categoryService.findByName(categoryType.getValue());
+        return category.getBooks()
+                .stream().map(BookListService::getBuild)
+                .collect(Collectors.toList());
     }
 
 }
