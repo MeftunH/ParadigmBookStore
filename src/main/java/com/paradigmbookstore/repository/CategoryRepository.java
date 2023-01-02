9b83e95e7f255186d7aca758e00a2d55bef2d576
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-03 02:56:21 +0300

    add find by name
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/repository/CategoryRepository.java
+++ src/main/java/com/paradigmbookstore/repository/CategoryRepository.java
@@ -3,7 +3,10 @@ package com.paradigmbookstore.repository;
 import com.paradigmbookstore.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+  Category findByName(String name);
 
 }
