4c5211986a4216f823b6f4b83aff7c0cc1cee8b7
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-03 06:07:53 +0300

    add finding resources to repository
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/repository/BookRepository.java
+++ src/main/java/com/paradigmbookstore/repository/BookRepository.java
@@ -1,10 +1,16 @@
 package com.paradigmbookstore.repository;
 
 import com.paradigmbookstore.model.Book;
+import com.paradigmbookstore.model.BookStatus;
 import com.paradigmbookstore.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Category> {
+import java.util.Arrays;
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Category> {
+  List<Book> findByStatus(BookStatus bookStatus);
+
+  List<Book> findByTitle(String title);
 }
