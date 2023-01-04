e82bd6ec5bef050b3e4e9d81e5e5cac39424df6e
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-04 23:58:45 +0300

    override handle method argument not valid exception
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paradigmbookstore/exception/GlobalExceptionHandler.java
@@ -1,13 +1,29 @@
 package com.paradigmbookstore.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
     public String handleException(Exception e) {
         return e.getMessage();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+      Map<String,String> errors = new HashMap<>();
+      ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+          errors.put(fieldError.getField(),fieldError.getDefaultMessage());
+      });
     }
 }
