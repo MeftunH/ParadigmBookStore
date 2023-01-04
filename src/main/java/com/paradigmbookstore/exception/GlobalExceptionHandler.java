00c16481c8ee25f89e3f0f3314494da605fdbf51
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-04 23:56:00 +0300

    set handle exception base functionality
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paradigmbookstore/exception/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.paradigmbookstore.exception;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
 }
