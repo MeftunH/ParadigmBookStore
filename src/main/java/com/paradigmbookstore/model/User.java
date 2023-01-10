524e04439965c0147ae62855c1b2fa6afe78339a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-10 22:58:49 +0300

    set unique username validation
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/model/User.java
+++ src/main/java/com/paradigmbookstore/model/User.java
@@ -2,13 +2,15 @@ package com.paradigmbookstore.model;
 import lombok.Data;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "user")
 public class User extends BaseEntity {
+    @Column(unique = true)
     private String username;
     private String password;
+
+
 }
