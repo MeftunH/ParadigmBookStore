2098f14357873d95338004241bb5195224227bf7
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-01 07:14:49 +0300

    add missed service annotation
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/service/UserDetailsServiceImpl.java
+++ src/main/java/com/paradigmbookstore/service/UserDetailsServiceImpl.java
@@ -1,15 +1,17 @@
 package com.paradigmbookstore.service;
 
 import com.paradigmbookstore.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService
 {
     private final UserService userService;
