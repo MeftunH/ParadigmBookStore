adc477d883bd1645683fdc449e05b3e2c4358812
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-01 03:01:28 +0300

    add find user by username functionality to user repository
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/repository/UserRepository.java
+++ src/main/java/com/paradigmbookstore/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.paradigmbookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+  Optional<User> findUserByUsername(String username);
 }
