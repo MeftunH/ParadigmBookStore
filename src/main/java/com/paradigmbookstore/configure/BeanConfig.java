0700f6c468e5730e1ba7918f80392771ac33adb8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-01 07:15:55 +0300

    declare object mapper bean
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/configure/BeanConfig.java
+++ src/main/java/com/paradigmbookstore/configure/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.paradigmbookstore.configure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,5 +10,10 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
     }
 }
