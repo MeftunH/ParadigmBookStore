73fae0f776dcd7d4ddfc95edaffca212367c6374
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-01 07:14:22 +0300

    add exception handling on security filter chain
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

--- src/main/java/com/paradigmbookstore/configure/SecurityConfig.java
+++ src/main/java/com/paradigmbookstore/configure/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.paradigmbookstore.configure;
 
+import com.paradigmbookstore.Security.JwtAccessDeniedHandler;
+import com.paradigmbookstore.Security.JwtAuthenticationEntryPoint;
+import com.paradigmbookstore.Security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,8 +11,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,42 +22,54 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    public SecurityConfig(JwtFilter jwtFilter, JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthenticationEntryPoint authenticationEntryPoint) {
+        this.jwtFilter = jwtFilter;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Bean
-    public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception
-    {
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
     @Bean
-    public SessionManagementConfigurer<HttpSecurity> securityFilterChain(HttpSecurity http) throws Exception
-    {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
                 .cors().and()
-                .authorizeRequests((auth)->{
+                .authorizeRequests((auth) -> {
                     auth.antMatchers("/api/admin").hasAnyAuthority("ADMIN");
-                    auth.antMatchers("/api/user").hasAnyAuthority("ADMIN","USER");
+                    auth.antMatchers("/api/user").hasAnyAuthority("ADMIN", "USER");
                     auth.anyRequest().authenticated();
                 })
                 .formLogin().disable()
                 .httpBasic().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
-   @Bean
-    public WebSecurityCustomizer webSecurityCustomizer()
-   {
-       return (web -> web.ignoring().antMatchers("/api/images/**"));
-   }
 
-   @Bean
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/api/images/**"));
+    }
+
+    @Bean
     public WebMvcConfigurer configurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-             registry.addMapping("/**")
-                     .allowedMethods("*");
+                registry.addMapping("/**")
+                        .allowedMethods("*");
             }
         };
-   }
+    }
 }
