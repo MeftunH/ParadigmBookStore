112a2d6aeeac8cd2fd13d93ec2e3117ae02862ab
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-01 05:45:08 +0300

    add security config class
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

---
+++ src/main/java/com/paradigmbookstore/configure/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.paradigmbookstore.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception
+    {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SessionManagementConfigurer<HttpSecurity> securityFilterChain(HttpSecurity http) throws Exception
+    {
+        return http
+                .csrf().disable()
+                .authorizeRequests((auth)->{
+                    auth.antMatchers("/api/admin").hasAnyAuthority("ADMIN");
+                    auth.antMatchers("/api/user").hasAnyAuthority("ADMIN","USER");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+   @Bean
+    public WebSecurityCustomizer webSecurityCustomizer()
+   {
+       return (web -> web.ignoring().antMatchers("/api/images/**"));
+   }
+
+   @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+             registry.addMapping("/**")
+                     .allowedMethods("*");
+            }
+        };
+   }
+}
